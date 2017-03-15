48929b4083a86ac1fbea74420f478b875f07832a
Xinyang <xinyzhang9@gmail.com>
2017-03-15 16:40:08 -0700

    refine cache

--- extractCommon.js
+++ extractCommon.js
@@ -1,7 +1,7 @@
 var data = require('./data/p2.json');
 var fs = require('fs');
 console.log(data.length);
-var common = ['春','花','月','夜','水','人','酒','风','雨','红'];
+var common = ['春','花','月','夜','水','人','酒','风','雨','红','好'];
 
 String.prototype.trunkTitle = function(){
 	var index = this.indexOf('】');
@@ -30,4 +30,4 @@ common.forEach(function(c){
 	match(c);
 })
 
-fs.writeFile('./data/p3.json', JSON.stringify(res_obj), 'utf8');
+fs.writeFile('./data/p4.json', JSON.stringify(res_obj), 'utf8');
